430de1b327d68491b2f48c8208a5cb86410bc1cf
Nicole-Scar <n.scarano@hotmail.com>
2025-12-19 11:05:43 +0000

    Add files via upload

--- api/validateOffer.js
+++ api/validateOffer.js
@@ -7,6 +7,10 @@ function normLower(v) {
 
 export default async function validateOffer(req, res) {
   try {
+    // ---------- ADD CORS HEADERS HERE ----------
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    // -------------------------------------------
     const fetch = (await import('node-fetch')).default; // dynamic import to prevent ESM crash
     const { contactId, booking_source } = req.query;
 
